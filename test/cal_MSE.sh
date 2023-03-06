b0aef6dac969eee3485c5a57ae12144ccc9c5a57
ZhenqiHe <72725463+HEZHENQI@users.noreply.github.com>
2023-03-06 23:28:47 +0800

    Update cal_MSE.sh

--- test/cal_MSE.sh
+++ test/cal_MSE.sh
@@ -1,2 +1,2 @@
-python /root/RePaint_SwinUNet_concat/MSE.py --pred_path=/root/Swin_unet/dataset/predicted_results_repaint_swinConcate --real_path=/root/Swin_unet/dataset/histology/data --mask_path=/root/Swin_unet/dataset/histology_mask
+python /root/RePaint_SwinUNet_concat/MSE.py --pred_path=PATH_TO_PREDICTED_RESULTS --real_path=PATH_TO_CLEAN_DATA --mask_path=PATH_TO_ARTIFACT_MASKS
 
