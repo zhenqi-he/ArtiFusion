1753f658082869e3ef2a0cc0176bc9936551dce1
ZhenqiHe <72725463+HEZHENQI@users.noreply.github.com>
2023-03-06 23:43:51 +0800

    Update sample.sh

--- model_training/sample.sh
+++ model_training/sample.sh
@@ -1,4 +1,4 @@
-export OPENAI_LOGDIR="/root/autodl-tmp/models_diffusion"
+export OPENAI_LOGDIR=""
 
 MODEL_FLAGS="--image_size 256 --num_channels 128 --num_res_blocks 3 --learn_sigma True"
 DIFFUSION_FLAGS="--diffusion_steps 4000 --noise_schedule linear"
@@ -6,4 +6,4 @@ TRAIN_FLAGS="--lr 1e-4 --batch_size 1"
 
 
 
-python scripts/image_sample.py --model_path /root/autodl-tmp/models_diffusion/ema_0.9999_080000.pt $MODEL_FLAGS $DIFFUSION_FLAGS --num_sample=1
+python scripts/image_sample.py --model_path PATH_TO_MODEL $MODEL_FLAGS $DIFFUSION_FLAGS --num_sample=1
