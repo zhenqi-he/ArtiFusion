139096f8cb5767d306e2e282259af07c28da36dc
ZhenqiHe <72725463+HEZHENQI@users.noreply.github.com>
2023-03-06 23:28:58 +0800

    Update cal_MSE.sh

--- test/cal_MSE.sh
+++ test/cal_MSE.sh
@@ -1,2 +1,2 @@
-python /root/RePaint_SwinUNet_concat/MSE.py --pred_path=PATH_TO_PREDICTED_RESULTS --real_path=PATH_TO_CLEAN_DATA --mask_path=PATH_TO_ARTIFACT_MASKS
+python MSE.py --pred_path=PATH_TO_PREDICTED_RESULTS --real_path=PATH_TO_CLEAN_DATA --mask_path=PATH_TO_ARTIFACT_MASKS
 
