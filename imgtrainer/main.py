5a5a4300871afd4092f250e572bb55b5044eb007
ZhenqiHe <72725463+HEZHENQI@users.noreply.github.com>
2023-03-07 00:39:57 +0800

    Update main.py

--- imgtrainer/main.py
+++ imgtrainer/main.py
@@ -4,7 +4,7 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     
-    parser.add_argument('--config_path', type=str, default='/root/autodl-tmp/TTMI/imgtrainer/config.yaml')
+    parser.add_argument('--config_path', type=str, default='./config.yaml')
     args = parser.parse_args()       
 
     mmtrainer = ImgTrainer(args.config_path)
