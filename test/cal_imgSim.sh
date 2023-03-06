73370a01aef29aa079913157e1707169e18e72f8
ZhenqiHe <72725463+HEZHENQI@users.noreply.github.com>
2023-03-06 23:29:32 +0800

    Update cal_imgSim.sh

--- test/cal_imgSim.sh
+++ test/cal_imgSim.sh
@@ -1,3 +1,3 @@
 pip install image-similarity-measures
 
-python /root/RePaint_SwinUNet_concat/cal_imgSim.py --pred_path=/root/Swin_unet/dataset/predicted_results_repaint_swinConcate --real_path=/root/Swin_unet/dataset/histology/data 
+python cal_imgSim.py --pred_path=PATH_TO_PREDICTED_RESULTS --real_path=PATH_TO_CLEAN_DATA
