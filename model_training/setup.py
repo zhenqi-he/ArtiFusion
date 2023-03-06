766589e0381dccb6af1cc5676e2a8523443e26c8
ZhenqiHe <72725463+HEZHENQI@users.noreply.github.com>
2023-03-06 23:18:47 +0800

    Update setup.py

--- model_training/setup.py
+++ model_training/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="improved-diffusion",
-    py_modules=["improved_diffusion"],
+    name="diffusion",
+    py_modules=["diffusion"],
     install_requires=["blobfile>=1.0.5", "torch", "tqdm"],
 )
