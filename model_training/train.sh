0f9c1c63230b2552f97c2259e0e036722e392b8d
ZhenqiHe <72725463+HEZHENQI@users.noreply.github.com>
2023-03-06 23:25:08 +0800

    Update train.sh

--- model_training/train.sh
+++ model_training/train.sh
@@ -1,4 +1,4 @@
-export OPENAI_LOGDIR="/root/autodl-tmp/models_ArtiFusion"
+export OPENAI_LOGDIR=""
 
 MODEL_FLAGS="--image_size 256 --num_channels 128 --num_res_blocks 3 --learn_sigma True"
 DIFFUSION_FLAGS="--diffusion_steps 4000 --noise_schedule linear"
