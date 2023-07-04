4a4d8de3d4106b35142ad485cc99463cde67bd53
ZhenqiHe <72725463+zhenqi-he@users.noreply.github.com>
2023-07-04 21:43:26 +0800

    Update train.sh

--- model_training/train.sh
+++ model_training/train.sh
@@ -1,6 +1,7 @@
 export OPENAI_LOGDIR=""
+export DATA_DIR=''
 
 MODEL_FLAGS="--image_size 256 --num_channels 128 --num_res_blocks 3 --learn_sigma True"
 DIFFUSION_FLAGS="--diffusion_steps 4000 --noise_schedule linear"
 TRAIN_FLAGS="--lr 1e-4 --batch_size 1"
-python scripts/image_train.py --data_dir /root/AACycleDataset/trainB $MODEL_FLAGS $DIFFUSION_FLAGS $TRAIN_FLAGS
+python scripts/image_train.py --data_dir $DATA_DIR $MODEL_FLAGS $DIFFUSION_FLAGS $TRAIN_FLAGS
